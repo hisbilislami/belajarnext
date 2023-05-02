451163fc6de02bcefb5eb7a5b9c7262728224ca8
hisbil islami <hisbil.prog@gmail.com>
2023-05-02 20:45:31 +0700

    adding pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,7 +11,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div
+          className="flex justify-center items-center w-100 p-3"
+          style={{ height: "50px", background: "orange" }}
+        >
+          <ul className="flex">
+            <li className="bg-white rounded p-2">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="bg-white rounded p-2 ml-2">
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="text-3xl text-center"> Home pages</h1>
+        </div>
       </main>
     </>
   );
